54e6c0d5e4a8bcb33d1ac9bd8465a5d2b3763295
lvladv <mr.lvladv@yandex.ru>
2021-06-27 18:31:13 +0300

    add avatar

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,11 +1,65 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Select from 'react-select';
+import Camera from 'assets/camera.svg';
+import Pencil from 'assets/pencil.svg';
 import styles from './styles.module.less';
 
 export const Profile = () => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [avatarURL, setAvatarURL] = useState<string>('');
+
+  const getDataUrlFromFile = (file: File | Blob): Promise<string> =>
+    new Promise((resolve) => {
+      const reader = new FileReader();
+
+      reader.addEventListener('load', () => {
+        resolve(reader.result as string);
+      });
+
+      reader.readAsDataURL(file);
+    });
+
+  function handleFileInputChanged(event: React.FormEvent<HTMLInputElement>) {
+    const [file] = event.target.files || [];
+
+    // добавить проверу размера файла
+    // handleChangeFile(file);
+
+    getDataUrlFromFile(file).then((fileUrl) => setAvatarURL(`${fileUrl}`));
+
+  }
+
+  const handleUploadAvatarBtnClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+  console.log(avatarURL);
   return (
     <div className={styles.wrapper}>
       <div className={styles.content}>
+        <div className={styles.profileWrapper}>
+          {avatarURL ? (
+            <img className={styles.img} src={avatarURL} alt="" />
+          ) : (
+            <Camera className={styles.defaultPhoto} />
+          )}
+
+          <div className={styles.pencil} onClick={handleUploadAvatarBtnClick}>
+            <form>
+              <input
+                hidden
+                onChange={handleFileInputChanged}
+                ref={fileInputRef}
+                type="file"
+                id="avatar"
+                accept=".png, .jpg, .jpeg"
+              />
+            </form>
+
+            <Pencil />
+          </div>
+        </div>
         <span className={styles.title}>Личная информация</span>
         <span className={styles.subtitle}>Имя</span>
         <input
